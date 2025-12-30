30497264fabd433680dc3a1b2e66f981e9456bd9
aanyaa20 <aanya.singhal20@gmail.com>
2025-12-31 02:39:33 +0530

    Simplified frontend structure and fixed backend server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,6 @@ import cors from "cors";
 import { ENV } from "./lib/env.js";
 // import { connectDB } from "./lib/db.js";
 // import { inngest, functions } from "./lib/inngest.js";
-
 // import chatRoutes from "./routes/chatRoutes.js";
 // import sessionRoutes from "./routes/sessionRoute.js";
 
@@ -19,11 +18,11 @@ const __dirname = path.resolve();
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+// app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("/api/chat", chatRoutes);
-app.use("/api/sessions", sessionRoutes);
+// app.use("/api/inngest", serve({ client: inngest, functions }));
+// app.use("/api/chat", chatRoutes);
+// app.use("/api/sessions", sessionRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
@@ -40,7 +39,7 @@ if (ENV.NODE_ENV === "production") {
 
 const startServer = async () => {
   try {
-    await connectDB();
+    // await connectDB();
     app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
     console.error(" Error starting the server", error);
