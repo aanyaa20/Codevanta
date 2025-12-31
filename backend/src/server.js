58c0e57ddcac0186a1053a644614b4e567088537
aanyaa20 <aanya.singhal20@gmail.com>
2025-12-31 22:45:15 +0530

    Commented out connectDB call

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,7 +39,7 @@ if (ENV.NODE_ENV === "production") {
 
 const startServer = async () => {
   try {
-    await connectDB();
+    // await connectDB();
     app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
     console.error(" Error starting the server", error);
